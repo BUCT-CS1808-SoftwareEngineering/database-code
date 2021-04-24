21c9772c7e5a23e6e20df852c2966237fca0794a
karthest <949654900@qq.com>
2021-04-24 18:26:34 +0800

    更改为promise风格

--- node_api/config.js
+++ node_api/config.js
@@ -1,9 +1,13 @@
+const mysql2 = require("mysql2/promise");
 var config = {
     database: 'test', 
-    username: 'root', 
+    user: 'root', 
     password: 'cs1808',
     host: 'localhost',
     port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 };
-
-module.exports = config;
+const pool = mysql2.createPool(config);
+module.exports = pool;
