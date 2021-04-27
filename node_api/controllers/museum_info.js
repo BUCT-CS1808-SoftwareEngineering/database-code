7a282895b17f01d67cfa45177cb05240d09f41c9
karthest <949654900@qq.com>
2021-04-27 12:17:42 +0800

    新增POSY参数校验

--- node_api/controllers/museum_info.js
+++ node_api/controllers/museum_info.js
@@ -1,30 +1,41 @@
 const APIError = require("../rest").APIError;
-const url = require("url")
-const pool = require("../config");
+const Url = require("url");
+const Joi = require("joi");
+const Pool = require("../config");
+
+
+const POST_SCHEME = Joi.object({
+    muse_Name:Joi.string().max(50).required(),
+    muse_Intro:Joi.string().required(),
+    muse_Location:Joi.number().less(10000000000).precision(6).required(),
+    muse_Address:Joi.string().max(100).required(),
+    muse_Opentime:Joi.string().max(50).required(),
+    muse_price:Joi.string().max(10).required(),
+    muse_class:Joi.string().max(50).required(),
+    muse_Ename:Joi.string().max(50).required(),
+})
+
 
 module.exports = {
     'GET /api/museum': async (ctx, next) => {
         ctx.rest();
     },
-    // body = {
-    //     muse_Name:"",
-    //     muse_Intro:"",
-    //     muse_Location:"",
-    //     muse_Address:"",
-    //     muse_Opentime:"",
-    //     muse_price:"",
-    //     muse_class:"",
-    //     muse_Ename:"",
-    // }
     'POST /api/museum': async (ctx, next) => {
-        const {muse_Name,muse_Intro,muse_Location,muse_Address,muse_Opentime,muse_price,muse_class,muse_Ename} = ctx.request.body;
-        const [rows,fields] = await pool.execute(`insert into 
-                                                \`museum info table\` 
-                                                (muse_Name,muse_Intro,muse_Location,muse_Address,muse_Opentime,muse_price,muse_class,muse_Ename) 
-                                                values 
-                                                (?,?,?,?,?,?,?,?)`,
-                                                [muse_Name,muse_Intro,muse_Location,muse_Address,muse_Opentime,muse_price,muse_class,muse_Ename]);
-        ctx.rest({...rows});
+        const {value,error} = POST_SCHEME.validate(ctx.request.body);
+
+        if(Joi.isError(error)){
+            throw new APIError("参数错误",error.message);
+        }
+        const {muse_Name,muse_Intro,muse_Location,muse_Address,muse_Opentime,muse_price,muse_class,muse_Ename} = value;
+        const query_string = `insert into 
+                            \`museum info table\` 
+                            (muse_Name,muse_Intro,muse_Location,muse_Address,muse_Opentime,muse_price,muse_class,muse_Ename) values 
+                            (?,?,?,?,?,?,?,?)`;
+        const rows = await Pool.execute(query_string,[muse_Name,muse_Intro,muse_Location,muse_Address,muse_Opentime,muse_price,muse_class,muse_Ename]);
+        ctx.rest({
+            code:"success",
+            info:"success",
+        });
     },
     'DELETE /api/museum': async (ctx, next) => {
         ctx.rest();
