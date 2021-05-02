5f50b901dcc20e12a35e45989772a8a0b26e6a31
karthest <949654900@qq.com>
2021-05-02 12:19:21 +0800

    新增添加展品方法

---
+++ node_api/controllers/collection.js
@@ -0,0 +1,51 @@
+const APIError = require("../rest").APIError;
+const Url = require("url");
+const Joi = require("joi");
+const Pool = require("../config");
+const MuseNameCache = require("../museum_cache");
+
+
+const POST_SCHEME = Joi.object({
+    muse_Name:Joi.string().max(50).required(),
+    col_Name:Joi.string().max(50).required(),
+    col_Intro:Joi.string().required(),
+    col_Photo:Joi.string().required(),
+})
+module.exports = {
+    'GET /api/collection': async (ctx, next) => {
+        ctx.rest();
+    },
+    'POST /api/collection': async (ctx, next) => {
+        const {value,error} = POST_SCHEME.validate(ctx.request.body);
+        if(Joi.isError(error)){
+            throw new APIError("参数错误",error.message);
+        }
+        var {muse_Name} = value;
+        const [row] = await Pool.execute(`select muse_ID from \`museum info table\` where muse_Name=?`,[muse_Name]);
+        //若museum info table里暂无此博物馆则存入缓存。
+        if(row.length === 0 ){
+            MuseNameCache.put(value,"collection info table");
+            ctx.rest({
+                code:"waiting",
+                info:"waiting",
+            })
+            return ;
+        }
+        delete value.muse_Name;
+        const muse_ID = row[0].muse_ID;
+        const query_string = `insert into 
+                            \`collection info table\` 
+                            SET ?`;
+        await Pool.query(query_string,{muse_ID:muse_ID,...value});
+        ctx.rest({
+            code:"success",
+            info:"success",
+        });
+    },
+    'DELETE /api/collection': async (ctx, next) => {
+        ctx.rest();
+    },
+    'PUT /api/collection': async (ctx, next) => {
+        ctx.rest();
+    }
+};
