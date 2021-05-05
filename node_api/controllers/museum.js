3bdc5706b678540623c305e722ccbec2dc8a3181
karthest <949654900@qq.com>
2021-05-05 21:11:53 +0800

    museum DELETE请求

--- node_api/controllers/museum.js
+++ node_api/controllers/museum.js
@@ -18,7 +18,9 @@ const POST_SCHEME = Joi.object({
     muse_class:Joi.string().max(50).required(),
     muse_Ename:Joi.string().max(50).required(),
 })
-
+const DELETE_SCHEME = Joi.object({
+    muse_ID:Joi.number().integer().required(),
+})
 
 module.exports = {
     'GET /api/museum/info': async (ctx, next) => {
@@ -37,11 +39,19 @@ module.exports = {
     },
     'GET /api/museum/num':async (ctx,next)=>{
         const get_num_sql = `select count(*) from \`museum info table\``;
-        var [result,err] = await Pool.query(get_num_sql);
-        ctx.rest({
-            code:"success",
-            info:Object.values(result[0])[0],
-        });
+        var [result,fields,err] = await Pool.query(get_num_sql);
+        if(!err){
+            ctx.rest({
+                code:"success",
+                info:Object.values(result[0])[0],
+            });
+        }
+        else{
+            ctx.rest({
+                code:"fail",
+                info:"fail",
+            });
+        }
     },
     'POST /api/museum/info': async (ctx, next) => {
         var {value,error} = POST_SCHEME.validate(ctx.request.body);
@@ -50,13 +60,7 @@ module.exports = {
             throw new APIError("参数错误",error.message);
         }
         const insert_sql = `insert into \`museum info table\` SET ?`;
-        const [row,err] = await Pool.query(insert_sql,value);
-        // console.log(row);
-        
-        ctx.rest({
-            code:"success",
-            info:"success",
-        });
+        const [row,fields,err] = await Pool.query(insert_sql,value);
         // 再解决缓存中的muse_Name匹配,插入news info table表
         let {muse_Name} = value;
         let waiting_array = MuseNameCache.get(muse_Name)
@@ -65,9 +69,24 @@ module.exports = {
                 await Pool.query(`insert into \`${element.table}\` SET ?`,{muse_ID:row.insertId,...element.query});
             });
         }
+        ctx.rest({
+            code:"success",
+            info:"success",
+        });
     },
     'DELETE /api/museum/info': async (ctx, next) => {
-        ctx.rest();
+        var {value,error} = DELETE_SCHEME.validate(ctx.request.body);
+        if(Joi.isError(error)){
+            throw new APIError("参数错误",error.message);
+        }
+        var {muse_ID} = value;
+        var [row,err] = await Pool.query(`delete from \`museum info table\` where muse_ID=?`,[muse_ID]);
+        if(!err){
+            ctx.rest({
+                code:"success",
+                info:"success",
+            })
+        }
     },
     'PUT /api/museum/info': async (ctx, next) => {
         ctx.rest();
