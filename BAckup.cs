1c8830bbb21eb1c0ae10ee81ff1e03e4657dcb08
Isatidis_Radix <81970872+Isatidis-Radix@users.noreply.github.com>
2021-05-17 18:32:35 +0800

    添加项目文件。

---
+++ BAckup.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WebApplication1
+{
+    public class BAckup
+    {
+        
+    }
+}
---
+++ Controllers/BAckupController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Threading.Tasks;
+using BAckup;
+using System.Threading;
+
+namespace BAckup.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class BAckupController : ControllerBase
+    {
+        
+        [HttpGet]
+        public string get()
+        {
+            return "1";
+        }
+
+        [HttpPost]
+        public string Test1()
+        {
+                Process p = new Process();
+                string filename = "msqldump";
+                string command = "-uroot -pA4W^:s3aW33p --single-transaction --flush-logs --master-data=2 --all-databases --delete-master-logs >/root/BACkupfile/date_$(date '+%Y%m%d%H%M').sql ";
+                p.StartInfo.FileName = filename;
+               //p.StartInfo.FileName = "C:/Windows/notepad.exe";
+                p.StartInfo.Arguments =command;
+                p.StartInfo.UseShellExecute = true;
+                try
+                {
+                    p.Start();
+                    p.WaitForExit();
+                }
+                catch (Exception e)
+                {
+                    return (-1).ToString();
+                }
+                return p.ExitCode.ToString();
+            
+            
+        }
+    }
+   
+}
