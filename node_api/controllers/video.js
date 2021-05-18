87c471c9cf8b44b0d99437798d4663f0bd15e134
swdlm <756093361@qq.com>
2021-05-18 08:07:15 +0800

    新增视频上传和模糊搜索接口

--- node_api/controllers/video.js
+++ node_api/controllers/video.js
@@ -2,27 +2,22 @@ const APIError = require("../rest").APIError;
 const Url = require("url");
 const Joi = require("joi");
 const Pool = require("../config");
-const asyncBusboy = require('async-busboy');
-// import asyncBusboy from 'async-busboy'
-
-const fs = require("fs");
-/*var express = require("express");
-var router = express.Router();
-var multer = require('multer');
-var upload = multer({dest:'upload/'})*/
+const fs = require('fs')
 
 const GET_SCHEME = Joi.object({
+    muse_ID:Joi.number().integer(),
+    muse_Name:Joi.string().max(50),
     pageIndex:Joi.number().integer().required(),
     pageSize:Joi.number().integer().required()
 })
 const POST_SCHEME = Joi.object({
-    // muse_ID:Joi.number().integer().required(),
-    // user_ID:Joi.number().integer().required(),
+    muse_ID:Joi.number().integer().required(),
+    user_ID:Joi.number().integer().required(),
     // video_Url:Joi.string().required(),
     // video_IfShow:Joi.boolean().required(),
     // video_Name:Joi.string().max(50).required(),
-    // video_Time:Joi.date().required(),
-    // video_Description:Joi.string().required(),
+    video_Time:Joi.date().required(),
+    video_Description:Joi.string().required(),
 })
 const DELETE_SCHEME = Joi.object({
     video_ID:Joi.number().integer().required(),
@@ -32,11 +27,10 @@ const PUT_SCHEME = Joi.object({
     video_ID:Joi.number().integer().required(),
     video_IfShow:Joi.bool().required(),
 })
-
-
-
+const getRegexpFromChinese = (museum_name)=>  museum_name.trim().split("").join("?");
 
 module.exports = {
+
     'GET /api/video': async (ctx, next) => {
         var query =Url.parse(ctx.request.url,true,true).query;
         var {value,error}=GET_SCHEME.validate(query);
@@ -47,7 +41,7 @@ module.exports = {
 
         const get_num_sql = `select count(*) from \`review video table\``;
         var [num_rows] = await Pool.query(get_num_sql);
-        const get_sql = `select * from \`review video table\` where video_IfShow='0' limit ? offset ?`;
+        const get_sql = `select * from \`review video table\` limit ? offset ?`;
         var [result] = await Pool.query(get_sql,[pageSize,(pageIndex-1)*pageSize]);
         ctx.rest({
             code:"success",
@@ -57,29 +51,62 @@ module.exports = {
             },
         });
     },
-    // 'POST /api/video': async (ctx, next) => {
-    //
-    //     const  {files,fields} = await asyncBusboy(ctx.req);
-    //     console.log(files,fields);
-    //     router.post('/upload', upload.single('file'), function(req, res, next){
-    //         console.log(req);
-    //         res.send({ret_code: '0'});
-    //     });
-    //
-    //     // var {value,error} = POST_SCHEME.validate(ctx.request.body);
-    //     // if(Joi.isError(error)){
-    //     //     throw new APIError("参数错误",error.message);
-    //     // }
-    //     // const insert_sql = `insert into \`review video table\` SET ?`;
-    //     // await Pool.query(insert_sql,value);
-    //     // ctx.rest({
-    //     //     code:"success",
-    //     //     info:"success",
-    //     // })
-    //     ctx.rest({a:1})
-    //
-    //
-    // },
+
+    'GET /api/video/search': async (ctx, next) => {
+        var query = Url.parse(ctx.request.url,true,true).query;
+        var {value,error} = GET_SCHEME.validate(query);
+        if(Joi.isError(error)){
+            throw new APIError("参数错误",error.message);
+        }
+        var {muse_ID,muse_Name,pageIndex,pageSize} = value;
+        if(typeof muse_ID == "undefined"){
+            const get_num_sql = `select count(*) from \`review video table\``;
+            var [num_rows] = await Pool.query(get_num_sql);
+            const get_id_sql = `select muse_ID from \`museum info table\` where muse_Name regexp ?`;
+            var [ID]= await Pool.query(get_id_sql,[getRegexpFromChinese(muse_Name)]);
+            const get_sql = `select * from \`review video table\` where muse_ID=${ID[0].muse_ID} limit ? offset ?`;
+            var [result] = await Pool.query(get_sql,[pageSize,(pageIndex-1)*pageSize]);
+        }
+        else{
+            const get_num_sql = `select count(*) from \`review video table\` where muse_ID=?`;
+            var [num_rows] = await Pool.query(get_num_sql,[muse_ID]);
+            const get_sql = `select * from \`review video table\` where muse_ID=? limit ? offset ?`;
+            var [result] = await Pool.query(get_sql,[muse_ID,pageSize,(pageIndex-1)*pageSize]);
+        }
+        ctx.rest({
+            code:"success",
+            info:{
+                num:Object.values(num_rows[0])[0],
+                items:result,
+            },
+        });
+    },
+
+    'POST /api/video': async (ctx, next) => {
+
+        var {value,error} = POST_SCHEME.validate(ctx.request.body);
+        if(Joi.isError(error)){
+            throw new APIError("参数错误",error.message);                      //
+        }
+        console.log(ctx.file,ctx.request.body);
+        fs.renameSync(ctx.file.path,`upload/${ctx.file.originalname}`)
+        ctx.rest(ctx.file)
+        var files=ctx.file
+        var videourl=files.path
+        var videoname=files.originalname
+        const insert_sql = `insert into \`review video table\` SET user_ID=${ctx.request.body.user_ID},
+                                                                   muse_ID=${ctx.request.body.muse_ID},
+                                                                   video_Url='${videourl}',
+                                                                   video_Name='${videoname}',
+                                                                   video_Time='${ctx.request.body.video_Time}',
+                                                                   video_Description='${ctx.request.body.video_Description}'`;
+        await Pool.query(insert_sql);
+
+
+
+    },
+
+
     'DELETE /api/video': async (ctx, next) => {
 
         var {value,error} = DELETE_SCHEME.validate(ctx.request.body);
