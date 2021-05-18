24c325cb0229cf8c111143080505d7afd8540983
karthest <949654900@qq.com>
2021-05-18 09:11:28 +0800

    增加请求头支持

--- node_api/app.js
+++ node_api/app.js
@@ -14,7 +14,7 @@ app.use(async (ctx, next) => {
         ctx.response.status=200;
         ctx.set('Access-Control-Allow-Origin', '*');
         ctx.set('Access-Control-Allow-Methods','PUT, POST, GET, DELETE, OPTIONS');
-        ctx.set('Access-Control-ALlow-Headers','X-Requested-With, Accept, Origin, Content-Type');
+        ctx.set('Access-Control-ALlow-Headers','X-Requested-With, Accept, Origin, Content-Type,Authorization');
         ctx.set('Access-Control-Allow-Credentials',true);
     }
     console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
